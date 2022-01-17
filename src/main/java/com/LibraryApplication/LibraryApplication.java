0102b22f009ac08b2857802660ca0b5111b48894
Tim Furdui <timfurdui@Tims-MacBook-Pro.local>
2022-01-17 14:38:14 -0800

    updated app.properties to be able to run on Mac

--- src/main/java/com/LibraryApplication/LibraryApplication.java
+++ src/main/java/com/LibraryApplication/LibraryApplication.java
@@ -1,7 +1,5 @@
 package com.LibraryApplication;
 
-import com.LibraryApplication.Library.model.LibraryCatalogItems.Book;
-import com.LibraryApplication.Library.model.LibraryCatalogItems.Movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,19 +12,4 @@ public class LibraryApplication {
         SpringApplication.run(LibraryApplication.class, args);
     }
 
-//    private static Book createBook() {
-//        Book book = new Book("9780393972832", "Herman Melville");
-//        book.setTitle("Moby Dick");
-//        book.setGenre("Adventure Fiction");
-//        book.setCatalogId(1);
-//        return book;
-//    }
-//
-//    private static Movie createMovie() {
-//        Movie movie = new Movie(132, "John McTiernan");
-//        movie.setTitle("Die Hard");
-//        movie.setGenre("Action");
-//        movie.setCatalogId(2);
-//        return movie;
-//    }
 }
